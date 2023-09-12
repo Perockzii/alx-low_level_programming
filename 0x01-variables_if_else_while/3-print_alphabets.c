535e36e7efed9aa81fcd0fc9621a47d716a9c723
Perockzii <perockzii@gmail.com>
2023-09-11 19:21:03 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include  <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char p;
+	char q;
+
+	p = 'a';
+	q = 'A';
+	while
+		(p <= 'z') {
+			putchar(p);
+			p++;
+		}
+	while
+		(q <= 'Z') {
+			putchar(q);
+			q++;
+		}
+	putchar('\n');
+	return (0);
+}
