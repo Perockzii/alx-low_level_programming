9ce7c7d34dc72aa3f1de421e16308849954250f2
Perockzii <perockzii@gmail.com>
2023-09-13 18:40:09 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @p: function parameter
+ * Return: q
+ */
+
+int print_last_digit(int p)
+{
+	int q;
+
+	q = p % 10;
+	if (p < 0)
+		q = -q;
+	_putchar(q + '0');
+	return (q);
+}
