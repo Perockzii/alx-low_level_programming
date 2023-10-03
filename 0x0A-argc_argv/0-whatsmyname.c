23a4822ad15c60be3c8ea72d87dad560b6edfb0f
Perockzii <perockzii@gmail.com>
2023-10-03 08:30:18 -0700

    attribute

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print program name
+ * @argc: arguement count
+ * @argv: arguemnts vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
