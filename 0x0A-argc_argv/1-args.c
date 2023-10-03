a9ef36e837f0cea704e898ae32fa8fc403f69f6b
Perockzii <perockzii@gmail.com>
2023-10-03 08:46:03 -0700

    silence

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguements
+ * @argc: arguements count
+ * @argv: arguments vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d/n", argc - 1);
+	return (0);
+}
