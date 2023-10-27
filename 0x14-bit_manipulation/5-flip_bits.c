2f90d7998127b5ab85b5a40e011af08eb85a5d3b
Perockzii <perockzii@gmail.com>
2023-10-26 19:43:27 -0700

    flip

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * flip_bits - function that returns the number of bits to be flipped
+ * @n: first number to compare
+ * @m: second number to compare
+ * Return: number of bits to be flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int result;
+	unsigned long int count;
+
+	result = n ^ m;
+
+	for (count = 0; result > 0;)
+	{
+		if ((result & 1) == 1)
+			count++;
+		result = result >> 1;
+	}
+	return (count);
+}
