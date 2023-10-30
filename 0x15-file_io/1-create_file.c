0b958209037703c67b6efbf78a7f16fa4b818086
Perockzii <perockzii@gmail.com>
2023-10-30 09:40:55 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
 
 	if (fd == -1)
 		return (-1);
@@ -23,6 +23,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	for (numletters = 0; text_content[numletters]; numletters++)
+		;
 	rwr = write(fd, text_content, numletters);
 
 	if (rwr == -1)
